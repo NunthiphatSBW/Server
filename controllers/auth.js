f7f7ebc548284e9cafe25cf256e49c942d8b0091
NunthiphatSBW <nunthiphat_so66@live.rmutl.ac.th>
2024-03-08 15:32:48 +0700

    AddUserEdited

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,52 @@
+// const { response } = require("express");
+
+// let mysql = require('mysql');
+// const env = require('../env.js');
+// const config = require('../dbconfig.js')[env];
+
+// const login = async (req, res = response) => {
+//   const { email, password } = req.body;
+
+//   //----------------------
+
+//   let dbcon = mysql.createConnection(config);
+
+//   const userDetails = "SELECT * FROM users where email = '" + email + "'";
+//   console.log(userDetails);
+
+//   dbcon.query(userDetails, function (err, user) {
+//     console.log(user);
+
+//     if (user.length > 0) {
+//       if (password !== user[0].password) {
+//         return res.status(400).json({
+//           msg: "User / Password are incorrect",
+//         });
+//       }
+
+//       res.status(200).json({ user })
+
+//       /*
+//             res.json({
+//               name: "Test User",
+//               token: "A JWT token to keep the user logged in.",
+//               msg: "Successful login",
+//             });
+//       */
+
+//     } else {
+//       // User not found
+//       return res.status(401).json({ message: "User not found !" })
+//     }
+//   })
+
+// };
+
+// module.exports = {
+//   login,
+// };
+
+//NewEdit:
 const { response } = require("express");
 
 let mysql = require('mysql');
@@ -6,6 +55,9 @@ const config = require('../dbconfig.js')[env];
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
+
+ // const email = req.body.email;
+ // const password  = req.body.password;
 
   //----------------------
 
@@ -18,6 +70,7 @@ const login = async (req, res = response) => {
     console.log(user);
 
     if (user.length > 0) {
+
       if (password !== user[0].password) {
         return res.status(400).json({
           msg: "User / Password are incorrect",
@@ -34,9 +87,11 @@ const login = async (req, res = response) => {
             });
       */
 
-    } else {
+    } else { //  if (user.length > 0) 
+
       // User not found
       return res.status(401).json({ message: "User not found !" })
+
     }
   })
 
