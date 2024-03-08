c5fe09a3f02c7ae2873fdacd2be2b35c065ee1ff
NunthiphatSBW <nunthiphat_so66@live.rmutl.ac.th>
2024-03-08 15:51:00 +0700

    edit3018

--- models/server.js
+++ models/server.js
@@ -9,10 +9,8 @@ class Server {
     this.paths = {
       auth: "/api/auth",
       homepage: "/api/homepage",
+      saveuser : "/api/saveuser",
     };
-
-    //paths.auth
-    //auth.homepage
 
     this.middlewares();
     this.routes();
@@ -23,27 +21,23 @@ class Server {
     this.app.use(express.json());
 
     // Pick up React index.html file
-    // this.app.use(
-    //   express.static(path.join(__dirname, "../../client/build"))
-    // );
+    this.app.use(
+      express.static(path.join(__dirname, "../../client/build"))
+    );
   }
 
   // Bind controllers to routes
   routes() {
     this.app.use(this.paths.auth, require("../routes/auth"));
     this.app.use(this.paths.homepage, require("../routes/homepage"));
-    // this.app.use(this.paths.users, require("../routes/users"));
+    this.app.use(this.paths.saveuser, require("../routes/saveuser"));
 
     // Catch all requests that don't match any route
-    this.app.get("*", (req, res) => {
-      /*
-      res.sendFile(
-        path.join(__dirname, "../../client/build/index.html")
-      );
-      */
-      res.send("<h1>Hello, This is API Back-end.</h1>");
-    });
-
+    // this.app.get("*", (req, res) => {
+    //   res.sendFile(
+    //     path.join(__dirname, "../../client/build/index.html")
+    //   );
+    // });
   }
 
   listen() {
--- models/servertest.js
+++ models/servertest.js
@@ -1,50 +1,104 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+// // const express = require("express");
+// // const cors = require("cors");
+// // const path = require("path");
 
-class Server {
-  constructor() {
-    this.app = express();
-    this.port = process.env.PORT || 3031;
-    this.paths = {
-      auth: "/api/auth",
-      homepage: "/api/homepage",
-      saveuser : "/api/saveuser",
-    };
+// // class Server {
+// //   constructor() {
+// //     this.app = express();
+// //     this.port = process.env.PORT || 3031;
+// //     this.paths = {
+// //       auth: "/api/auth",
+// //       homepage: "/api/homepage",
+// //       saveuser : "/api/saveuser",
+// //     };
 
-    this.middlewares();
-    this.routes();
-  }
+// //     this.middlewares();
+// //     this.routes();
+// //   }
 
-  middlewares() {
-    this.app.use(cors());
-    this.app.use(express.json());
+// //   middlewares() {
+// //     this.app.use(cors());
+// //     this.app.use(express.json());
 
-    // Pick up React index.html file
-    this.app.use(
-      express.static(path.join(__dirname, "../../client/build"))
-    );
-  }
+// //     // Pick up React index.html file
+// //     this.app.use(
+// //       express.static(path.join(__dirname, "../../client/build"))
+// //     );
+// //   }
 
-  // Bind controllers to routes
-  routes() {
-    this.app.use(this.paths.auth, require("../routes/auth"));
-    this.app.use(this.paths.homepage, require("../routes/homepage"));
-    this.app.use(this.paths.saveuser, require("../routes/saveuser"));
+// //   // Bind controllers to routes
+// //   routes() {
+// //     this.app.use(this.paths.auth, require("../routes/auth"));
+// //     this.app.use(this.paths.homepage, require("../routes/homepage"));
+// //     this.app.use(this.paths.saveuser, require("../routes/saveuser"));
 
-    // Catch all requests that don't match any route
-    // this.app.get("*", (req, res) => {
-    //   res.sendFile(
-    //     path.join(__dirname, "../../client/build/index.html")
-    //   );
-    // });
-  }
+// //     // Catch all requests that don't match any route
+// //     // this.app.get("*", (req, res) => {
+// //     //   res.sendFile(
+// //     //     path.join(__dirname, "../../client/build/index.html")
+// //     //   );
+// //     // });
+// //   }
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log("Server running on port: ", this.port);
-    });
-  }
-}
+// //   listen() {
+// //     this.app.listen(this.port, () => {
+// //       console.log("Server running on port: ", this.port);
+// //     });
+// //   }
+// // }
 
-module.exports = Server;
+// // module.exports = Server;
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+
+// class Server {
+//   constructor() {
+//     this.app = express();
+//     this.port = process.env.PORT || 3031;
+//     this.paths = {
+//       auth: "/api/auth",
+//       homepage: "/api/homepage",
+//       saveuser : "/api/saveuser",
+//     };
+
+//     this.middlewares();
+//     this.routes();
+//   }
+
+//   middlewares() {
+//     this.app.use(cors());
+//     this.app.use(express.json());
+
+//     // Pick up React index.html file
+//     this.app.use(
+//       express.static(path.join(__dirname, "../../client/build"))
+//     );
+//   }
+
+//   // Bind controllers to routes
+//   routes() {
+//     this.app.use(this.paths.auth, require("../routes/auth"));
+//     this.app.use(this.paths.homepage, require("../routes/homepage"));
+//     this.app.use(this.paths.saveuser, require("../routes/saveuser"));
+
+//     // Catch all requests that don't match any route
+//     // this.app.get("*", (req, res) => {
+//     //   res.sendFile(
+//     //     path.join(__dirname, "../../client/build/index.html")
+//     //   );
+//     // });
+//   }
+
+//   listen() {
+//     this.app.listen(this.port, () => {
+//       console.log("Server running on port: ", this.port);
+//     });
+//   }
+// }
+
+// module.exports = Server;
